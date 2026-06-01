aea3dda073a38208da4e35b65cb20be0b69d4100
Al Rouby <mustafa.elrouby@hotmail.com>
2026-06-01 06:39:20 +0300

    Before Review with AI

---
+++ date_engineering_freecodecamp/pipeline/airflow/dags/elt_dag.py
@@ -0,0 +1,63 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from docker.types import Mount
+from airflow.operators.python_operators import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.operators.docker import DockerOperator
+import subprocess
+
+default_arg = {
+    'owner': 'airflow'
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+}
+
+
+def run_elt_script():
+    script_path = "/opt/airflow/elt/elt_script.py"
+    result = subprocess.run(["python", script_path],
+                            capture_output=True, text=True)
+    if result.returncode != 0:
+        raise Exception(f"Script failed with error: {result.stderr}")
+    else:
+        print(result.stdout)
+
+
+dag = DAG(
+    'elt_and_dbt',
+    default_args=default_arg,
+    description='An ELT workflow with dbt',
+    start_date=datetime(2023, 10, 28),
+    catchup=False,
+)
+
+t1 = PythonOperator(
+    task_id="run_elt_script",
+    python_callable=run_elt_script,
+    dag=dag
+)
+
+t2 = DockerOperator (
+    task_id="dbt_run",
+    image='ghcr.io/dbt-labs/dbt-postgres:1.8.2'
+    command=[
+        "run",
+        "--profiles-dir",
+        "/root",
+        "--project-dir",
+        "/dbt"
+    ],
+    auto_remove=True,
+    docker_url="unix://var/run/docker.sock",
+    network_mode="bridge",
+    mounts=[
+        Mount(source='/users/justinchau/Development/elt/custom_postgres',
+              target='/dbt', type='bind'),
+        Mount(source='/Users/justinchau/.dbt',
+              target='/root', type='bind')
+    ],
+    dag=dag
+)
+
+t1 >> t2
